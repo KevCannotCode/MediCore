1c751a7b1f23999355e3285a3a51d0d90627cff8
Koszarek11 <klemkosz@gmail.com>
2024-10-09 12:54:12 -0500

    Still need to fix 'route' module

---
+++ react-front-end/src/Patient_Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Patient_Login(){
+    return(
+        <div>
+            <h1>Login for Patient</h1>
+        </div>
+    )
+}
+
+export default Patient_Login;
