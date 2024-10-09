d783fefdf2a1886c63904644ccb966858a439496
Koszarek11 <klemkosz@gmail.com>
2024-10-09 16:24:34 -0500

    Created Login fields

--- react-front-end/src/Patient_Login.js
+++ react-front-end/src/Patient_Login.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./styles/Patient_Login.css";
 
 function Patient_Login(){
     return(
-        <div>
+        <div className="center-container">
             <h1>Login for Patient</h1>
+            <div class= "PLogin">
+                
+                <form>
+                    <label>Username</label>
+                    <input type="text"/>
+                    <label>Password</label>
+                    <input type="password"/>
+                    <button>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
