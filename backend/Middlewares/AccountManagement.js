36d9de2e792b3d474897e05c2d9c11d865330c92
Kevin <kevinumba3007@gmail.com>
2024-10-28 23:50:53 -0500

    KU 10/28/2024 - Added role assignment feature

--- backend/Middlewares/AccountManagement.js
+++ backend/Middlewares/AccountManagement.js
@@ -8,53 +8,66 @@ const UserModel = require("../Models/User");
     If the user exists, the function will delete the user from the database. 
     If the user does not exist, the function will return a 403 status code with a message.
 */
-const deleteAccount = () => {
-    return async (req, res, next) => {
-        try {
-            // Get jwt payload
-            const email = req.body.email;
-            if (!email) {
-                return res.status(404).json({ message: 'User not found' });
-            }   
-            // Query user
-            const user = await UserModel.deleteOne({ email: email });
+const deleteAccount = async (req, res, next) => {
+    try {
+        // Get jwt payload
+        const email = req.body.email;
+        if (!email) {
+            return res.status(404).json({ message: 'User not found' });
+        }   
+        // Query user
+        const user = await UserModel.deleteOne({ email: email });
 
-            if (user.deletedCount != 1) {
-                return res.status(403).json({ message: 'User does not exist' });
-            }
-            next();
-        } catch (err) {
-            return res.status(504).json({ message: 'Check deleteAccount in Auth' });
+        if (user.deletedCount != 1) {
+            return res.status(403).json({ message: 'User does not exist' });
         }
+        next();
+    } catch (err) {
+        return res.status(504).json({ message: 'Check deleteAccount in Auth' });
     }
 }
 
-const createAccount = async (req, res) => {
+const createAccount = async (req, res, next) => {
     try {
         const { name, email, password, role } = req.body;
         const user = await UserModel.findOne({ email });
         if (user) {
-            return res.status(409)
-                .json({ message: 'User is already exist!', success: false });
+            return res.status(409).json({ message: 'User is already exist!', success: false });
         }
         const userModel = new UserModel({ name, email, password, role });
         userModel.password = await bcrypt.hash(password, 10);
         await userModel.save();
-        res.status(201)
-            .json({
-                message: "Account created successfully",
-                success: true
-            })
+        next();
     } catch (err) {
-        res.status(500)
-            .json({
-                message: "Internal server errror",
-                success: false
-            })
+        res.status(500).json({ message: "Internal server errror", success: false });
+    }
+}
+
+const assignRole = async (req, res, next) => {
+    try {
+        // Get verify email
+        const { email, newRole } = req.body;
+        if (!email) {
+            return res.status(404).json({ message: 'User not found' });
+        }   
+
+        const updatedUser = await UserModel.findOneAndUpdate(
+            { email: email },
+            { role: newRole },
+            { new: true, runValidators: true }
+        );
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found', success: false });
+        }
+
+        next();
+    } catch (err) {
+        return res.status(504).json({ message: 'Check assignRole in AccountManagement' });
     }
 }
 
 module.exports = {
     deleteAccount,
-    createAccount
+    createAccount,
+    assignRole
 };
--- backend/Routes/AdminRouter.js
+++ backend/Routes/AdminRouter.js
@@ -5,28 +5,35 @@ const Auth = require('../Middlewares/Auth');
 const AccountManagement = require('../Middlewares/AccountManagement');
 const AuthValidation = require('../Middlewares/AuthValidation');
 
-// router.use(Auth.verifyRole('admin'));
+router.use(Auth.verifyRole('admin'));
 
-router.get('/', Auth.verifyRole('admin'), (req, res) => {
-    console.log('---- logged in user detail ---', req.user);
+router.get('/', (req, res) => {
+    console.log('---- Get Admin ---', req.user);
     res.status(200).json([
         { message: 'This is Admin page' }
     ])
 });
 
-router.post('/deleteUser', Auth.verifyRole('admin'), AccountManagement.deleteAccount, (req, res) => {
-    console.log('---- logged in user detail ---', req.user);
+router.post('/deleteUser', AccountManagement.deleteAccount, (req, res) => {
+        var t = "---- delete User Post method ---";
+    console.log(t);
     res.status(200).json([
-        { message: 'email : ' + 
-            req.body.email + ' was successfully deleted' }
+        { message: req.body.email + ' was successfully deleted' }
     ])
 });
 
-router.post('/createUser', AuthValidation.signupValidation, Auth.verifyRole('admin'), AccountManagement.createAccount, (req, res) => {
-    console.log('---- logged in user detail ---', req.user);
+router.post('/createUser', AuthValidation.signupValidation, AccountManagement.createAccount, (req, res) => {
+    console.log('---- Create User Post ---');
     res.status(200).json([
-        { message: 'email : ' + 
-            req.body.email + ' was successfully created!' }
+        { message: req.body.email + ' was successfully created!' }
+    ])
+});
+
+router.post('/assignRole', AccountManagement.assignRole, (req, res) => {
+    console.log('---- Assign Role POST ---', req.user);
+    res.status(200).json([
+        { message: 
+            req.body.email + ' was successfully changed to ' + req.body.newRole }
     ])
 });
 module.exports = router;
